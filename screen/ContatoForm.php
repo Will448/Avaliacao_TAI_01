a64fbf787a517dbfb9ae830cbc965b4cc022af70
Will <willianbongiovanni311@gmail.com>
2022-05-10 18:51:03 -0300

    Update(Form contato)-OK

--- screen/ContatoForm.php
+++ screen/ContatoForm.php
@@ -37,7 +37,7 @@ if (!empty($_POST)) {
 }
 ?>
 
-<nav class="navbar navbar-dark bg-primary">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
    <a class="navbar-brand" href="#"></a>
 <a class="navbar-brand" href="../index.php">Sis Agenda</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
@@ -58,55 +58,69 @@ if (!empty($_POST)) {
   </div>
 </nav>
 
-    <h2>Formulário do Contato</h2>
+<h2 style= "text-align: center">Formulário do Contato</h2>
+    
     <form action="./ContatoForm.php" method="post">
     <input type="hidden" name="id_contato" value="<?php echo !empty($result->id_contato) ? $result->id_contato : "" ?>" /><br>
           <div class="form-row">
     <div class="col">
+    <p style="color: rgb(255, 136, 0)"><font size="4">  Nome</font></p>
       <input type="text" class="form-control" placeholder="Nome" name="nome" value="<?php echo !empty($result->nome) ? $result->nome : "" ?>"/><br>
     </div>
     <div class="col">
+    <p style="color: rgb(255, 136, 0)"><font size="4">  Sobrenome</font></p>
       <input type="text" class="form-control" placeholder="Sobrenome" name="sobrenome" value="<?php echo !empty($result->sobrenome) ? $result->sobrenome : "" ?>" /><br>
     </div>
    </div>
    <div class="form-row">
     <div class="col-auto my-1">
+    <p style="color: rgb(255, 136, 0)"><font size="4">1º telefone</font></p>
       <input type="text" class="form-control" placeholder="Primeiro telefone" name="telefone1" value="<?php echo !empty($result->telefone1) ? $result->telefone1 : "" ?>" /><br>
       </div>
     <div class="col-auto my-1">
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-        <option selected name="tipo_telefone1" value="<?php /*if($tipo_telefone1 == $tipo_telefone)*/ echo !empty($result->tipo_telefone1) ? $result->tipo_telefone1 : "" ?>">Tipo Telefone 01</option><br>
-        <option value="1">Casa</option>
-        <option value="2">Celular</option>
-        <option value="3">Comercial</option>
-        <option value="4">Principal</option>
+    <p style="color: rgb(255, 136, 0)"><font size="4">Tipos: Casa, Celular, Comercial, Principal </font></p>
+      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="tipo_telefone1" id="inlineFormCustomSelect" aria-label="Default select example" value="<?php echo !empty($result->tipo_telefone1) ? $result->tipo_telefone1 : "" ?>">
+        <option></option><br>
+        <option value="Casa">Casa</option>
+        <option value="Celular">Celular</option>
+        <option value="Comercial">Comercial</option>
+        <option value="Principal">Principal</option>
       </select>
     </div>
     <div class="form-row">
     <div class="col-auto my-1">
-      <input type="text" class="form-control" placeholder="Telefone 02" name="telefone2" value="<?php echo !empty($result->telefone2) ? $result->telefone2 : "" ?>" />><br>
+    <p style="color: rgb(255, 136, 0)"><font size="4">1º telefone</font></p>
+      <input type="text" class="form-control" placeholder="Telefone 02" name="telefone2" value="<?php echo !empty($result->telefone2) ? $result->telefone2 : "" ?>" /><br>
       </div>
-    <div class="col-auto my-1">
-      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect">
-        <option selected name="tipo_telefone2" value="<?php echo !empty($result->tipo_telefone2) ? $result->tipo_telefone2 : "" ?>">Tipo Telefone 02</option><br>
-        <option value="1">Casa</option>
-        <option value="2">Celular</option>
-        <option value="3">Comercial</option>
-        <option value="4">Principal</option>
+      <div class="col-auto my-1">
+    <p style="color: rgb(255, 136, 0)"><font size="4">Tipos: Casa, Celular, Comercial, Principal  </font></p>
+      <select class="custom-select mr-sm-2" id="inlineFormCustomSelect" name="tipo_telefone2" id="inlineFormCustomSelect" aria-label="Default select example" value="<?php echo !empty($result->tipo_telefone2) ? $result->tipo_telefone2 : "" ?>">
+        <option></option><br>
+        <option value="Casa">Casa</option>
+        <option value="Celular">Celular</option>
+        <option value="Comercial">Comercial</option>
+        <option value="Principal">Principal</option>
       </select>
     </div>
     </div>
   <br>
   <div class="col my-1">
+  <p style="color: rgb(255, 136, 0)"><font size="4">   Email</font></p>
       <input type="email" class="form-control" id="colFormLabel" placeholder="nome@exemplo.com" name="email" value="<?php echo !empty($result->email) ? $result->email : "" ?>" /><br>
     </div>
     </div>
-   
-    <button type= "submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Salvar</button>    
+   <br>
+   <button type= "submit" class="btn btn-success"><i class="fa-solid fa-floppy-disk"></i> Salvar</button>    
+
 </form>
 <br>
-<button type="button" class="btn btn-info" href="ContatoList.php"><i class="fa-solid fa-circle-arrow-left"></i> Voltar</button>
-  
+<a href="../index.php"><button type="button" class="btn btn-info" >Voltar   <i class="fa-solid fa-circle-arrow-left"></i></button></a>
+
+<style>
+body {
+    background-color: #e6f7ff;
+}
+
 </body>
 
 </html>
